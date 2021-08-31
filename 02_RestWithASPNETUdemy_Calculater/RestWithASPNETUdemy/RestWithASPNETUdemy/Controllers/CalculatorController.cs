eefddbbafb44cb2e1f5ef7aa26dcebd2e8d0581e
Tarcisio <tarcisiotecpc@gmail.com>
2021-08-30 22:34:36 -0300

    Implementing Calculator

---
+++ 02_RestWithASPNETUdemy_Calculater/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        public readonly ILogger<CalculatorController> _logger;
+        public  CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sun/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumbe)
+        {
+            if (Isnumeric(firstNumber) && Isnumeric(secondNumbe))
+
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumbe);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string secondNumbe)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool Isnumeric(string secondNumbe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
