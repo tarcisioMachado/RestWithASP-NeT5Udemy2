e4745801c8ab9caae8d6fbaed5a9b2cae0479562
Tarcisio <tarcisiotecpc@gmail.com>
2021-09-06 16:54:33 -0300

    Working with four Verbs

---
+++ 02_RestWithASPNETUdemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/Person.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    public class Person : Controller
+    {
+        private readonly ILogger<Person> _logger;
+
+        public Person(ILogger<Person> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}
